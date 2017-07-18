ddee56e08cdfeccd16898f26dc7fbce1c71b41e3
szeregnyiv <szeregnyiv@gain.nyme.hu>
2017-07-18 09:30:33 +0200

    HelloServlet jetty maven jsp

---
+++ src/main/java/hu/neuron/demoapp/HelloServlet.java
@@ -0,0 +1,32 @@
+package hu.neuron.demoapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/helloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("<h1>Hello Servlet</h1>");
+	}
+
+}
